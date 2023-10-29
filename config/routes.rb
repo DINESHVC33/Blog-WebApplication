fa7c28b40faf1978a77915dceab5ff9b779dc0fa
Dinesh VC <dineshvc33@gmail.com>
2023-10-29 16:52:38 +0530

    post added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :topics
+
+  resources :topics do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
