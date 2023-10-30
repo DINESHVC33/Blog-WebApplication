bcb97c66988013b43ac9121113daddc9e1f4d1fd
Dinesh VC <dineshvc33@gmail.com>
2023-10-30 06:41:53 +0530

    added navbar and home page!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  root  to:'home#index'
   get'/posts' , to: 'posts#all_posts' , as: 'all_posts'
   resources :topics do
     resources :posts do
